ce590ae53d55cf6265420ba504f4c484d842384d
Rodrigo Sepúlveda <seplveda@gmail.com>
2020-08-12 23:29:07 -0400

    init

---
+++ sketch.js
@@ -0,0 +1,49 @@
+let snake;
+let rez = 10;
+let food;
+let w;
+let h;
+
+function setup() {
+	createCanvas(800, 800);
+	w = floor(width / rez);
+	h = floor(height / rez);
+	frameRate(20);
+	snake = new Snake();
+	foodLocation();
+	
+
+}
+
+function foodLocation() {
+	let x = floor(random(w));
+	let y = floor(random(h));
+	food = createVector(x, y);
+}
+
+function keyPressed() {
+	if(keyCode === LEFT_ARROW){
+		snake.setDir(-1, 0);
+	} else if (keyCode === RIGHT_ARROW) {
+		snake.setDir(1, 0);
+	} else if (keyCode === DOWN_ARROW) {
+		snake.setDir(0, 1);
+
+	} else if (keyCode === UP_ARROW){
+		snake.setDir(0, -1);
+	}
+}
+function draw() {
+	scale(rez)
+	background(38, 70, 83);
+	if (snake.eat(food)){
+		foodLocation();
+	}
+	snake.update();
+	snake.show();
+
+	noStroke();
+	fill(233, 196, 106)
+	rect(food.x, food.y, 1, 1);
+
+}
---
+++ snake.js
@@ -0,0 +1,39 @@
+class Snake {
+
+	constructor() {
+		this.body = [];
+		this.body[0] = createVector(0, 0);
+		this.xdir = 0;
+		this.ydir = 0;
+
+	}
+
+	setDir(x, y) {
+		this.xdir = x;
+		this.ydir = y;
+	}
+
+	update() {
+
+		this.body[0].x += this.xdir;
+		this.body[0].y += this.ydir;
+
+	}
+
+	eat(pos) {
+		let x = this.body[0].x;
+		let y = this.body[0].y;
+		if (x == pos.x && y == pos.y){
+			return true;
+		}
+		return false;
+
+	}
+
+	show() {
+		fill(231, 111, 81)
+		noStroke();
+		rect(this.body[0].x, this.body[0].y, 1, 1);
+
+	}
+}
